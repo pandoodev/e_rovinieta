763a6032f971bab2876bde5d8471133367e28923
Dorin Bujor <dorinash11@gmail.com>
2017-04-03 11:40:19 +0300

    edit/add/remove profiles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions, ActionConst } from 'react-native-router-flux';
 import LoginForm from './components/login/LoginForm';
 import AddToCart from './components/menu/store/rov_shopping/AddToCart';
 import AddProfile from './components/menu/profile/AddProfile';
+import EditProfile from './components/menu/profile/EditProfile';
 import RovignetteShopMain from './components/menu/store/rov_shopping/RovignetteShopMain';
 import BuyBridgeTicket from './components/menu/store/bridge_shopping/BuyBridgeTicket';
 import Categories from './components/menu/store/bridge_shopping/Categories';
@@ -23,6 +24,7 @@ const scenes = Actions.create(
 			<Scene key="dashboard" component={Dashboard} initial/>
 			<Scene key="profiles" component={Profile} />
 			<Scene key="add_profile" component={AddProfile} />
+			<Scene key="edit_profile" component={EditProfile} />
 			<Scene key="cars" component={Cars} />
 			<Scene key="account_settings" component={AccountSettings} />
 			
