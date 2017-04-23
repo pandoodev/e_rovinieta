b642f55a4f7a0ba9d576f5ff0a4b9d3626088be3
Dorin Bujor <dorinash11@gmail.com>
2017-04-23 15:13:36 +0300

    removing file rename

---
+++ src/components/login/PushController.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+var PushNotification = require('react-native-push-notification');
+
+
+
+class PushController extends Component {
+
+    componentDidMount() {
+        PushNotification.configure({
+            onNotification: function (notification) {
+            },
+
+        });
+
+    }
+    render()
+    {
+        return null;
+    }
+
+
+}
+
+export default PushController;
