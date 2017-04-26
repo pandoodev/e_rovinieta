a87b86a181380a4e0a6221f2daa80a594cd1bf4c
pandoodev <dev@pandoo.ro>
2017-04-26 14:49:03 +0300

    Merge before commit

--- android/app/src/main/java/com/untrr.rovinieta/MainActivity.java
+++ android/app/src/main/java/com/untrr.rovinieta/MainActivity.java
@@ -1,4 +1,4 @@
-package com.untrr.erovinieta;
+package com.untrr.rovinieta;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "untrr.erovinieta";
+        return "untrr.rovinieta";
     }
 }
--- android/app/src/main/java/com/untrr.rovinieta/MainApplication.java
+++ android/app/src/main/java/com/untrr.rovinieta/MainApplication.java
@@ -1,4 +1,4 @@
-package com.untrr.erovinieta;
+package com.untrr.rovinieta;
 
 import android.app.Application;
 import android.util.Log;
