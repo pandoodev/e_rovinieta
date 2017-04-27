99208857f792713ce39a8ed6e6ef391ef5b0b430
MihaiBaba34 <mihaibaba34@gmail.com>
2017-04-27 20:23:04 +0300

    Merge before commit

--- index.android.js
+++ index.android.js
@@ -2,4 +2,4 @@ const React = require('react-native');
 const { AppRegistry, } = React;
 import App from './src/App';
 
-AppRegistry.registerComponent('untrr.erovinieta', () => App);
+AppRegistry.registerComponent('untrr.vinieta', () => App);
